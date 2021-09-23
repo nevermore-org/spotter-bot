7a941fab883a1bc410b22508c354556827f57f7b
Krystof Liskovec <krystof.liskovec@gmail.com>
2021-09-23 21:37:01 +0200

    Added BaseFractal interface

---
+++ src/Model/Guildwars/BaseFractal.ts
@@ -0,0 +1,4 @@
+export default interface BaseFractal {
+    name: string;
+    level: number;
+}
--- src/Model/Guildwars/Fractal.ts
+++ src/Model/Guildwars/Fractal.ts
@@ -1,11 +1,12 @@
-export default interface Fractal {
-    id: number;
-    icon: string;
-    name: string;
-    description: string;
-    requirement: string;
-    locked_text: string;
-    type: string;
-    flags: string[];
-    recommended: boolean;
+import BaseFractal from "./BaseFractal";
+
+export default interface Fractal extends BaseFractal {
+    id: number | null;
+    icon: string | null;
+    description: string | null;
+    requirement: string | null;
+    locked_text: string | null;
+    type: string | null;
+    flags: string[] | null;
+    recommended: boolean | null;
 }
