34eac5746f4d5fabd1368e5645a7384e7e8b8d25
Ashmogh <draconirx@gmail.com>
2021-10-04 17:00:17 +0200

    Try luxon package for dates

--- src/Discord/Controller/PingController.ts
+++ src/Discord/Controller/PingController.ts
@@ -1,11 +1,12 @@
 import { CommandInteraction } from "discord.js";
 import DiscordControllerInterface from "../../Model/Discord/DiscordControllerInterface";
+import { DateTime } from "luxon";
 
 /**
  * /ping
  */
 export default class PingController implements DiscordControllerInterface {
     public handleInteraction = async (interaction: CommandInteraction): Promise<void> => {
-        interaction.reply(`${new Date()}`);
+        interaction.reply(`${DateTime.now().setZone('UTC+2')}`);
     }
 }
