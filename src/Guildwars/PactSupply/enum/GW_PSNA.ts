52795782b7c502ed52ba7c2bdd236b6da3cb2d61
Ashmogh <draconirx@gmail.com>
2021-10-11 21:57:24 +0200

    Fix some PSNA waypoints

--- src/Guildwars/PactSupply/enum/GW_PSNA.ts
+++ src/Guildwars/PactSupply/enum/GW_PSNA.ts
@@ -5,7 +5,7 @@ export const PACT_AGENTS: string[] = ["Mehem", "Fox", "Derwena","Yana", "Katelyn
 // who even starts the week with a sunday ffs...
 export const AGENT_LOCATIONS: string[][] = [
     ['[&BIgHAAA=]', '[&BH8HAAA=]', '[&BH4HAAA=]', '[&BKsHAAA=]', '[&BJQHAAA=]', '[&BH8HAAA=]','[&BIYHAAA=]'],
-    ['[&BEwDAAA=]', '[&BEgAAAA=]', '[&BMIBAAA=]', '[&BE8AAAA=]', '[&BMMCAAA=]', '[&BLkCAAA=]','[&BDoBAAA=]'],
+    ['[&BEwDAAA=]', '[&BEgAAAA=]', '[&BMIBAAA=]', '[&BE8AAAA=]', '[&BMwCAAA=]', '[&BNMCAAA=]','[&BDoBAAA=]'],
     ['[&BKYBAAA=]', '[&BBkAAAA=]', '[&BKYAAAA=]', '[&BIMAAAA=]', '[&BNUGAAA=]', '[&BJIBAAA=]','[&BC0AAAA=]'],
     ['[&BNIEAAA=]', '[&BKgCAAA=]', '[&BP0CAAA=]', '[&BP0DAAA=]', '[&BJsCAAA=]', '[&BBEDAAA=]','[&BO4CAAA=]'],
     ['[&BIMCAAA=]', '[&BGQCAAA=]', '[&BDgDAAA=]', '[&BF0GAAA=]', '[&BHsBAAA=]', '[&BEICAAA=]','[&BIUCAAA=]'],
