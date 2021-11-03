9513f4c46101cfe99a1c1c79c9ce52ffe376557f
Ashmogh <draconirx@gmail.com>
2021-11-03 20:58:03 +0100

    Refactor fractals
    Pretty big change, it's now using the DB to get the achievement data, I also changed a lot of the data types that are passed inside the functions

--- src/Discord/View/ViewFractals.ts
+++ src/Discord/View/ViewFractals.ts
@@ -5,66 +5,52 @@ import View from "./View";
 import { EMBED_ID } from "./enum/EMBED_ID";
 import { DateTime } from "luxon";
 import BaseFractal from "../../Model/Guildwars/BaseFractal";
+import { FractalInfo, InstabFractalInfo } from "../../Model/Guildwars/FractalInfo";
 
 export default class ViewFractals extends View {
     thumbnail: string = THUMBNAILS.FRACTAL;
-    //todayStr: string;
+    titlesMap: Record<string, string> = {'daily': `Daily T4s + Recs`, 'cm': `Daily CMs`};
 
     /**
-     * ////Should set all properties that are displayable
+     *
      */
     constructor() {
         super();
-        // this.todayStr = DateTime.utc().setLocale('en-gb').toLocaleString();
     }
 
     /**
-     * Set embed to daily T4s + recs
-     * @param levels
-     * @param instabilities
-     * @param recommended
+     * Sets the fractal embed; is called for all command options
+     * @param commandOption 
+     * @param fractals 
+     * @param recs 
      * @returns 
      */
-    public setEmbedToDaily = (levels: BaseFractal[][], instabilities: string[][][], recommended: string[]): ViewFractals => {
-        
-        //Notice the first argument. It's an id that is being set to an embed.
-        //Embeds in our context are basically the main "container" for all the properties. E. g. Buttons can't exist without an embed.
-        const fractalsEmbed = this.createEmbed(EMBED_ID.FRACTALS, `Daily T4s + Recs`, this.thumbnail);
+    public setEmbeds = (commandOption: string, fractals: InstabFractalInfo[], recs: FractalInfo[] = []): ViewFractals => {
+        const fractalsEmbed = this.createEmbed(EMBED_ID.FRACTALS, this.titlesMap[commandOption], this.thumbnail);
         let offset = 0;
 
-        instabilities.forEach((instability, loopIndex) => {
-            fractalsEmbed.addField(`${levels[loopIndex][0].name}`, this.formatInstability(instability));
+        fractals.forEach(fractal => {
+            fractalsEmbed.addField(`${fractal.name}`, this.formatInstabilities(fractal.instabilities));
 
             // if the fractal type has two variants, insert one padding field and one with the second variant
-            if (instability.length > 1){
+            if (fractal.instabilities.length > 1){
                 fractalsEmbed.addField('\u180E', '\u180E\n**OR**\n\u180E', true);
-                fractalsEmbed.addField('\u180E', this.formatInstability(instability, 1), true);
+                fractalsEmbed.addField('\u180E', this.formatInstabilities(fractal.instabilities, 1), true);
                 
                 fractalsEmbed.fields[offset].inline = true;
             }
             
             // number of fields for each row we have - 3 if there are two data fields and one padding
-            offset += instability.length > 1 ? 3 : 1;
-        })
+            offset += fractal.instabilities.length > 1 ? 3 : 1;
+                        
+        });
 
-        fractalsEmbed.addField(`Recommended fractals`, recommended.join('\n'));
-        return this
-    }
+        // basically if you want to show recommended fractals or not
+        if(recs.length > 0) {
+            fractalsEmbed.addField(`Recommended fractals`, `${recs.map(rec => [rec.levels[0], rec.name].join(" ")).sort().reverse().join("\n")}`);
+        }
 
-    /**
-     * Create new embed and set it to Daily CMs
-     * @param levels
-     * @param instabilities
-     */
-
-    public setEmbedToCMs = (levels: BaseFractal[][], instabilities: string[][][]): ViewFractals => {
-        const CMsEmbed = this.createEmbed(EMBED_ID.FRACTALS, `Daily CMs`, this.thumbnail);
-        
-        instabilities.forEach((instability, index) => {
-            CMsEmbed.addField(`${levels[index][0].name}`, this.formatInstability(instability));
-        })
-
-        return this;        
+        return this;
     }
     
     /**
@@ -72,8 +58,8 @@ export default class ViewFractals extends View {
      * @param instability
      * @param variantIndex - index of a variant for a given fractal
      */
-     public formatInstability(instab: string[][], variantIndex: number = 0) {
-        return [0, 1, 2].map(fractalIndex => instab[variantIndex][fractalIndex]).join('\n');
+     public formatInstabilities(instabilities: string[][], variantIndex: number = 0) {
+        return [0, 1, 2].map(fractalIndex => instabilities[variantIndex][fractalIndex]).join('\n');
     }
 
     /**
--- src/Guildwars/Fractals/FractalAPI.ts
+++ src/Guildwars/Fractals/FractalAPI.ts
@@ -1,11 +1,9 @@
 import axios from "axios"
 import { DateTime } from "luxon";
 import { AchievementResponse } from "../../Model/Guildwars/Achievement";
-import BaseFractal from "../../Model/Guildwars/BaseFractal";
-import Fractal from "../../Model/Guildwars/Fractal";
+import { FractalInfo } from "../../Model/Guildwars/FractalInfo";
 import { getIdsFromCollection } from "../../Mongo/Mongo";
 import { GW_API_URL } from "../General/enum/GW_API_URL";
-import GW_FRACTALS from "./enum/GW_FRACTALS";
 import { GW_INSTABILITIES, GW_INST_NAMES } from "./enum/GW_INSTABILITIES";
 
 export default class FractalAPI {
@@ -18,72 +16,28 @@ export default class FractalAPI {
         const fractalAchievements: AchievementResponse[] = dailyResponse.data.fractals;
 
         const fractalIds: number[] = fractalAchievements.map((achiev: AchievementResponse) => achiev.id);
-        const fractalResponse = await axios.get(`${GW_API_URL.ACHIEVEMENTS}?ids=${fractalIds}`);
-        const fractals: Fractal[] = fractalResponse.data;
+        const fractalsDB = await getIdsFromCollection(fractalIds, 'achievements');
 
-        //const fractalsDB = await getIdsFromCollection(fractalIds, 'achievements');
-        //const fractalsDaily = fractalsDB?.filter( fractal => ["DailyT4", "Recommended"].includes(fractal.fractal_info.type));
-        //console.log(fractalsDaily);
-
-        const parsedFractals = this.parseRecommendedFractals(fractals);
-
-        return parsedFractals;
+        const fractalsDaily = fractalsDB?.filter( fractal => fractal.fractal_info && ["DailyT4", "Recommended"].includes(fractal.fractal_info.type));
+        const fractalsDailyInfo: FractalInfo[] = fractalsDaily ? fractalsDaily.map(achiev => achiev.fractal_info) : [];
+        
+        return fractalsDailyInfo;
     }
-    /**
-     * Yes. These numskulls don't return the name of the recommended fractal.
-     * @param fractals 
-     */
-    private parseRecommendedFractals(fractals: Fractal[]) {
-        return fractals.map((fractal: Fractal) => {
-            fractal.recommended = false;
-            if (fractal.name.includes("Daily Recommended")) {
-                const fractalLevel: number = Number(fractal.name.split(" ")[3]);
-                // might be bit too many levels here
-                const fractalName = GW_FRACTALS.find(fLevel => fLevel.level == fractalLevel)?.name;
-
-                fractal.name = `${fractalLevel} ${fractalName}`;
-                fractal.recommended = true;
-            }
-            return fractal;
-        });
-    }
-
-    /**
-    * Returns all types of fractals that match fractalName as BaseFractal object  
-    * Right now filters only T4's
-    * @param fractalName
-    */
-    private getLevelsByName(fractalName: string) {
-        return GW_FRACTALS.filter(level => level.name === fractalName && level.level > 75);
-    }
-
-
-    /**
-     * Returns array of arrays which contain the daily fractals' levels
-     * Complexity is not ideal (O(3n) altogether), should be possible in one loop only
-     * @param fractals 
-     */
-    public getDailyT4Levels(fractals: Fractal[]) {
-        // don't need the "Daily Tier 4" part
-        // hardcoded T4s' positions in array (6, 10, 14)
-        const names: string[] = [fractals[6].name, fractals[10].name, fractals[14].name].map(name => name.slice(13));
-        const levels = names.map(name => this.getLevelsByName(name));
-
-        return levels; // -> [[{name: Underground, level: 78},{name: Underground, level: 84}], [{...}], [{...}]]
-    }
-
 
     /**
      * Converts array of daily fractals to their matching array of instabilities
      * @param levels
      */
-    public getInstabilities(levels: BaseFractal[][]) {
+    public getInstabilities(fractals: FractalInfo[]) {
         const today = DateTime.utc().ordinal;
 
         // replaces each level with list of instability indices; then replaces those with their names
         let getInstabilitiesForLevel = (level: number) => (GW_INSTABILITIES[level][today]).map(index => GW_INST_NAMES[index]);
 
-        return levels.map(fractalType => fractalType.map(level => getInstabilitiesForLevel(level.level)));
+        return fractals.map( fractal => {
+            const instabilities = fractal.levels.map(level => getInstabilitiesForLevel(level));
+            return {... fractal, instabilities};
+        });     
     }
 
 }
